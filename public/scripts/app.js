35be26794604b267725da6e1f4f218edff557aba
conmart <connor.a.martinelli@gmail.com>
2017-09-06 09:45:13 -0700

    modal for individual toilets is working

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,18 +7,20 @@ console.log('jQuery connected');
 
 
   $('.modal').modal();
-
-  function initMap() {
-    console.log('initMap running');
-    let map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 2,
-          center: new google.maps.LatLng(37.78, -122.44),
-        });
-        console.log('end')
-  }
-
   initMap();
+
+
+
+
 
 
   // end of document ready
 })
+
+
+function initMap() {
+  let map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: new google.maps.LatLng(37.78, -122.44),
+      });
+}
