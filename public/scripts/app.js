0d2a754b4b0fc5e975fc448219bd21ebe1846140
conmart <connor.a.martinelli@gmail.com>
2017-09-05 17:18:34 -0700

    made maps work

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,6 +6,19 @@ $(document).ready(function () {
 console.log('jQuery connected');
 
 
+  $('.modal').modal();
+
+  function initMap() {
+    console.log('initMap running');
+    let map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 2,
+          center: new google.maps.LatLng(37.78, -122.44),
+        });
+        console.log('end')
+  }
+
+  initMap();
+
 
   // end of document ready
 })
